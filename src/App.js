28757216873f567d32a7f847a702ca3562a22020
samuella <ezeogesamuella@gmail.com>
2024-12-18 14:49:13 +0100

    my designs for suggested box block

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+
+import './App.css';
+import RightSide from './Component/rightSide';
+
 function App() {
   return (
     <div className="App">
-      
+   <div className="rightSide">
+  <RightSide/>
+   </div>
     </div>
   );
 }
