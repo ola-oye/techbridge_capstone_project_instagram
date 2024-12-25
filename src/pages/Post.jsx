2e2af90a98ee5b07c85cf911c6979ae704f0b3da
ola-oye <olasunkanmioyetunji4@gmail.com>
2024-12-25 21:25:29 +0100

    Added story update feature

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,11 +1,16 @@
 import RightSide from "../components/rightSide";
 import Postcard from "../components/Postcard";
+import Stories from "../components/stories";
 import "../styles/post.css";
 
 function Post() {
   return (
     <main className="main">
-      <Postcard />
+      <div className="wrapper">
+        <Stories />
+        <Postcard />
+      </div>
+
       <RightSide />
     </main>
   );
